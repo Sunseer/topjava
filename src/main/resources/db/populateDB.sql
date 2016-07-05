31f80755eff323033a60f0de55474d547d1c58f8
Sunseer <playvsme@gmail.com>
2016-07-05 14:53:43 +0300

    HW03

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -11,3 +12,7 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories) VALUES
+(100000, '2016-07-01 11:00:00','завтрак', 500),
+(100000,'2016-07-01 14:52:00', 'обед', 1600);
